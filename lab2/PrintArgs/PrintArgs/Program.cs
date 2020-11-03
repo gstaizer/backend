3bc740229f88c3c2846275d0819333662f1e18f9
NextGenen <sergey.starikov@elephant-games.com>
2020-11-03 08:18:00 +0300

    upd 1 2

---
+++ lab2/PrintArgs/PrintArgs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string NoParametres = "No parameters were specified!", numberOfArg = "Number of arguments: {0}", arguments = "Arguments: {0}";
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine(NoParametres);
+            }
+            else
+            {
+                Console.WriteLine(numberOfArg, args.Length);
+                Console.Write(arguments, String.Join(" ", args));
+            }
+        }
+    }
+}
---
+++ lab2/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string incorrectNumber = "Incorrect number of arguments!", example = "Usage RemoveDuplicates.exe < input string>";
+
+            if (args.Length != 1)
+            {
+                Console.WriteLine(incorrectNumber);
+                Console.WriteLine(example);
+                return;
+            }
+
+            string line = args[0];
+            Dictionary<char, bool> exitingChars = new Dictionary<char, bool>();
+
+            foreach (char ch in line)
+            {
+                if (!exitingChars.ContainsKey(ch))
+                {
+                    exitingChars[ch] = true;
+                    Console.Write(ch);
+                }
+            }
+        }
+    }
+}
